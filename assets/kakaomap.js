5832efa2cb4b23c725e36035b581c4586cd5ec08
Junwoo Hwang <zzulu.hwang@gmail.com>
2021-10-03 16:35:46 +0900

    Add: clipboard.js

--- assets/kakaomap.js
+++
@@ -1,7 +0,0 @@
-const targetLatLng = new kakao.maps.LatLng(35.825237470682744, 129.29089412186622)
-const mapContainer = document.getElementById('map')
-const mapOption = { center: targetLatLng, level: 4 }
-const map = new kakao.maps.Map(mapContainer, mapOption)
-const markerPosition = targetLatLng;
-const marker = new kakao.maps.Marker({ position: markerPosition });
-marker.setMap(map);
